0603b326a0cfaa9804c5cb2036d2598d7f0b1af6
Sudo-oy <oy.sudodata@gmail.com>
2025-10-03 21:31:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, jsonify
+import psycopg2
 
 app = Flask(__name__)
 
 @app.route("/")
 def health():
     return jsonify({"status": "ok", "message": "DevOps pipeline demo running!"})
+
+@app.route("/db")
+def db_check():
+    try:
+        conn = psycopg2.connect(
+            host="db",
+            database="mydb",
+            user="admin",
+            password="admin"
+        )
+        cur = conn.cursor()
+        cur.execute("SELECT version();")
+        db_version = cur.fetchone()
+        cur.close()
+        conn.close()
+        return jsonify({"database": "connected", "version": db_version})
+    except Exception as e:
+        return jsonify({"database": "error", "details": str(e)})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
